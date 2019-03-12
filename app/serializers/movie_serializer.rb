d7d367d77c5aacbaa6b25d5201fc507889f361cb
Marcelo Jr <marcelo.jr63@gmail.com>
2019-03-12 20:08:04 -0300

    Add photo_url and youtube_id to MovieSerializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,3 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :main_genre, :release_year, :director, :description
+  attributes :id, :title, :main_genre, :release_year, :director, :description, :photo_url, :youtube_id
 end
